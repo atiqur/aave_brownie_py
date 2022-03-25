f79c66780af2acb1f1efc58a1d4f846cd95cf999
Atiqur Rahman <atiqur.india@gmail.com>
2022-03-25 23:47:31 +0530

    Borrow DAI

--- scripts/aave_borrow.py
+++ scripts/aave_borrow.py
@@ -3,7 +3,7 @@ from scripts.helpful_scripts import get_account
 from brownie import config, network, interface
 from web3 import Web3
 
-amount = Web3.toWei(0.1, "ether")
+AMOUNT = Web3.toWei(0.1, "ether")
 
 
 def main():
@@ -13,14 +13,43 @@ def main():
         get_weth()
     lending_pool = get_lending_pool()
     # Approve sending out ERC20 tokens
-    approve_erc20(amount, lending_pool.address, erc20_address, account)
+    approve_tx = approve_erc20(AMOUNT, lending_pool.address, erc20_address, account)
     print("Depositing...")
     tx = lending_pool.deposit(
-        erc20_address, amount, account.address, 0, {"from": account}
+        erc20_address, AMOUNT, account.address, 0, {"from": account}
     )
     tx.wait(1)
     print("Deposited!")
     borrowable_eth, total_debt = get_borrowable_data(lending_pool, account)
+    print("Let's borrow!")
+    # DAI in terms of ETH
+    dai_eth_price = get_asset_price(
+        config["networks"][network.show_active()]["dai_eth_price_feed"]
+    )
+    amount_dai_to_borrow = (1 / dai_eth_price) * (borrowable_eth * 0.95)
+    # borrowable_eth -> borrowable_dai * 95%
+    print(f"We are going to borrow {amount_dai_to_borrow} DAI")
+    # Now we will borrow!
+    dai_address = config["networks"][network.show_active()]["dai_token"]
+    borrow_tx = lending_pool.borrow(
+        dai_address,
+        Web3.toWei(amount_dai_to_borrow, "ether"),
+        1,
+        0,
+        account.address,
+        {"from": account},
+    )
+    borrow_tx.wait(1)
+    print("We borrowed some DAI")
+    get_borrowable_data(lending_pool, account)
+
+
+def get_asset_price(price_feed_address):
+    dai_eth_price_fees = interface.AggregatorV3Interface(price_feed_address)
+    latest_price = dai_eth_price_fees.latestRoundData()[1]
+    converted_latest_price = Web3.fromWei(latest_price, "ether")
+    print(f"The DAI/ETH price is {converted_latest_price}")
+    return float(converted_latest_price)
 
 
 def get_borrowable_data(lending_pool, account):
